f184d08462a38a1e70c69af2eef6adff26d58140
HectorOC-e <osoriohector89@gmail.com>
2022-12-27 21:44:46 -0600

    Agregar archivos de proyecto.

---
+++ ConceptosPOO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace ConceptosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //realizarTarea();
+
+            var miVariable = new { Nombre = "Juan", Edad = 19 };
+
+
+            var miOtraVariable = new { Nombre = "Ana", Edad = 25 };
+
+            miVariable = miOtraVariable;
+
+            Console.WriteLine($"Nombre: {miVariable.Nombre} Edad: {miVariable.Edad}");
+        }
+        static void realizarTarea()
+        {
+            Punto origen = new Punto();
+
+            Punto destino = new Punto(150, 90);
+
+            double distancia = origen.DistanciaHasta(destino);
+
+            Console.WriteLine($"La distancia entre los dos puntos es de : {distancia}");
+            Console.WriteLine($"Número de objetos creados: {Punto.ContadorDeObjetos()}");
+        }
+    }
+}
---
+++ ConceptosPOO/Punto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPOO
+{
+    class Punto
+    {
+        private int x, y;
+        private static int contadorDeObjetos = 0;
+
+        public Punto()
+        {
+            this.x = 0;
+            this.y = 0;
+
+            contadorDeObjetos++;
+            //Console.WriteLine("Este es el constructor por defecto");
+        }
+
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            
+            contadorDeObjetos++;
+            //Console.WriteLine($"Coordenada X: {x}\nCoordena Y: {y}");
+        }
+
+        public double DistanciaHasta(Punto otroPunto)
+        {
+            int xDif = this.x - otroPunto.x;
+            int ydif = this.y - otroPunto.y;
+
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(ydif, 2));
+            
+            return distanciaPuntos;
+
+        }
+
+        public static int ContadorDeObjetos() => contadorDeObjetos;    }
+}
